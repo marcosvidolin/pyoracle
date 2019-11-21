44e87335bac1562ba44a7126cbd4fcf92de838f5
Marcos Vidolin <mvidolin@ciandt.com>
2019-11-21 10:03:52 -0300

    query example

--- api/server.py
+++ api/server.py
@@ -1,10 +1,12 @@
 import os
 import cx_Oracle
 from flask import Flask
+import json
 
 db_user = os.environ.get('DB_USERNAME', 'USERNAME_NOSET')
 db_password = os.environ.get('DB_PASSWORD', 'PASSWORD_NOSET')
 db_connect = os.environ.get('DB_CONNECT_DESCRIPTOR', "localhost:1521/foo")
+query = os.environ.get('QUERY', "SELECT 'Hello, World from Oracle DB!' FROM DUAL")
 service_port = port=os.environ.get('PORT', '5000')
 
 app = Flask(__name__)
@@ -12,12 +14,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     connection = cx_Oracle.connect(db_user, db_password, db_connect)
-    cur = connection.cursor()
-    cur.execute("SELECT 'Hello, World from Oracle DB!' FROM DUAL")
-    col = cur.fetchone()[0]
-    cur.close()
+    cursor = connection.cursor()
+    cursor.execute(query)
+
+    result = []
+    for row in cursor:
+        result.append(row)
+
+    cursor.close()
     connection.close()
-    return col
+    return json.dumps(result)
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=int(service_port))
